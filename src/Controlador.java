0ec3e0776ad5ccbedde587082cd9aecf4ea0982e
yavi7006 <javigomez5599@gmail.com>
2025-01-28 19:39:21 +0100

    toma de forma

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,11 +1,33 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public class Controlador {
 
     @FXML
     private MenuBar barraMenu;
+
+    @FXML
+    private VBox biblioteca;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private VBox centro;
+
+    @FXML
+    private VBox edicion;
+
+    @FXML
+    private Label tituloArchivo;
+
+    @FXML
+    private MediaView viewMedia;
 
     private Stage stage;
 
