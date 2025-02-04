a087ac98dd9d1e48d86e325484ddfb13bcefae81
yavi7006 <javigomez5599@gmail.com>
2025-02-04 14:24:59 +0100

    cambios

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("biblioMulti.fxml"));
+        Parent root = loader.load();
+        Controlador controlador = loader.getController();
+        controlador.setStage(primaryStage);
+        primaryStage.setTitle("Biblioteca");
+        primaryStage.setScene(new Scene(root,600,600));
+        primaryStage.show();
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
